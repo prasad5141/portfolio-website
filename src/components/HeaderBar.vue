1f1d59a19867235527bf559757ae7166969ee4f0
Saiprasad Malkapuram <prasad.51415@gmail.com>
2022-12-28 02:49:07 +0530

    Header bar implememnted

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="landing-page">
+<span>
+  <div class="header-nav-bar">
+    <h1>SAIPRASAD MALKAPURM</h1>
+    <nav>
+      <a href="/html/">Home</a> 
+      <a href="/css/">About Me</a> 
+      <a href="/js/">Resume</a> 
+      <a href="/python/">Contact Me</a>
+    </nav>
+
+  </div>
+  </span>
+</div>
+</template>
+<script></script>
+<style>
+h1 {
+  text-align: left;
+}
+.header-nav-bar {
+  color: rgb(255, 255, 255);
+  margin-top: 75px;
+  margin-left: 125px;
+}
+.landing-page{
+  overflow: auto; 
+  height: 500px;
+  background-color:rgb(36, 38, 59) ;
+}
+</style>
