a6c4a1b2ef673e9f4b8c51e04e53054b0a84d0b8
Saiprasad Malkapuram <prasad.51415@gmail.com>
2023-05-07 02:48:17 +0530

    hello world app removed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderBar from "./components/HeaderBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderBar,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +21,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: -22px;
+  margin-left: -8px;
+  margin-right: -8px;
 }
 </style>
