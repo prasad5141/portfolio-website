66793475ffdf112d842d20c5bdd8817efa83bf86
Saiprasad Malkapuram <prasad.51415@gmail.com>
2023-05-08 00:10:55 +0530

    added fontawesome and added bootstrap to vue project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+//
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faTwitter,faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret,faTwitter,faGithub,faLinkedin)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
